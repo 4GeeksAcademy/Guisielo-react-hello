fdf3f24e6526dabc998c8a4616a00e45d59f3074
Guisielo <155173663+Guisielo@users.noreply.github.com>
2026-05-01 23:56:30 +0000

    Creacion de componente Card

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,7 +1,15 @@
 export const Card = () => {
     return (
-        <>
-            <h1>Hola mundo</h1>
-        </>
+        <div>
+            <div className="card" style={{width: "18rem"}}>
+                <img src="https://images.pexels.com/photos/10967612/pexels-photo-10967612.jpeg" class="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <hr />
+                    <a href="#" class="btn btn-primary">Find Out More!</a>
+                </div>
+            </div>
+        </div>
     )
 }
