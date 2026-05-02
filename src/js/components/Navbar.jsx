0362c559fea6a42071e33790c22950a0aab590e9
Guisielo <155173663+Guisielo@users.noreply.github.com>
2026-05-02 22:13:21 +0000

    Actualizacion de componente Navbar.jsx

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,11 +1,14 @@
 export const Navbar = () => {
     return (
         <div>
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container">
                     <a className="navbar-brand" href="#">Start Bootstrap</a>
-                    <div className="d-flex">
-                        <ul className="navbar-nav flex-row gap-3">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>    
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-lg-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
